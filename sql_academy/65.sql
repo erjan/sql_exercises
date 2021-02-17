8c423b04a66a3cb860b198c84e79d761d6e1173e
Erjan <erjan@users.noreply.github.com>
2021-02-17 23:44:26 +0600

    Create 65.sql

---
+++ sql_academy/65.sql
@@ -0,0 +1,10 @@
+
+'''
+Необходимо вывести рейтинг для комнат, которые хоть раз арендовали, как среднее значение рейтинга отзывов округленное до целого вниз.
+'''
+
+select room_id, floor(avg(rating)) as rating from reservations inner join rooms on reservations.room_id = rooms.id 
+inner join reviews on reviews.reservation_id = reservations.id
+group by room_id 
+having count(reservations.id) >=1
+
