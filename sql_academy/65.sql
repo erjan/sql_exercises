48400796139a8b24a4e94676e8c88a8ab046c5e5
Erjan <erjan@users.noreply.github.com>
2021-02-17 23:47:37 +0600

    made the right comment out

--- sql_academy/65.sql
+++ sql_academy/65.sql
@@ -1,7 +1,7 @@
 
-'''
-Необходимо вывести рейтинг для комнат, которые хоть раз арендовали, как среднее значение рейтинга отзывов округленное до целого вниз.
-'''
+
+-- Необходимо вывести рейтинг для комнат, которые хоть раз арендовали, как среднее значение рейтинга отзывов округленное до целого вниз.
+
 
 select room_id, floor(avg(rating)) as rating from reservations inner join rooms on reservations.room_id = rooms.id 
 inner join reviews on reviews.reservation_id = reservations.id
