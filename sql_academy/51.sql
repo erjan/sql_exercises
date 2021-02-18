571f32a3c08ca2cbe6ee3f11f09b711329d845b8
Erjan <erjan@users.noreply.github.com>
2021-02-18 23:19:19 +0600

    Update 51.sql

--- sql_academy/51.sql
+++ sql_academy/51.sql
@@ -1,5 +1,4 @@
 -- Добавьте товар с именем "Cheese" и типом "food" в список товаров (Goods).
-
 INSERT INTO Goods (good_id, good_name, type)
 
-SELECT COUNT(*) + 1 , 'Cheese', 2 from goods
+SELECT COUNT(*) + 1 , 'Cheese', (SELECT good_type_id from GoodTypes where good_type_name='food') from goods
