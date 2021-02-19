ccc915b9a05ec3a1a248948f35f31ca8eed2d09e
Erjan <erjan@users.noreply.github.com>
2021-02-20 00:14:51 +0600

    Delete 61.sql

--- window_functions/61.sql
+++
@@ -1,5 +0,0 @@
-
--- Выведите список комнат, которые были зарезервированы в течение 12 недели 2020 года.
-
-select rooms.*, start_date, end_date from reservations inner join rooms on rooms.id = reservations.room_id 
-where week(start_date, 1) = 12 and year(start_date) = 2020
