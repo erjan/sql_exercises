2bce3b3182cd8ac2f596ee4ce5904368b1b7c5bd
Erjan <erjan@users.noreply.github.com>
2021-02-23 14:52:54 +0600

    Update 60.sql

--- sql_academy/60.sql
+++ sql_academy/60.sql
@@ -1,14 +1,14 @@
 -- Print the identifiers of the teachers who taught at least once for the entire time in each of the eleventh grades.
 
--- BAD SOLUTION!
 with help as (
 select teacher, classroom, name from schedule inner join class on class.id = schedule.class where class.name like '11%')
 
 
 select distinct h1.teacher from help h1 inner join help h2 on h1.teacher = h2.teacher where  h1.name != h2.name
 
+-- note: "условие "не меньше ОДНОГО раза" не требует дополнительной проверки на количество"
 
--- BETTER SOLUTION with having()
+-- bad SOLUTION with having() - no need to use having!
 
 with help as (
 select teacher, classroom, name from schedule inner join class on class.id = schedule.class where class.name like '11%')
