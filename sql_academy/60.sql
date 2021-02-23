91cb3bc0dad0b93ad42c088f661ec0212bae255a
Erjan <erjan@users.noreply.github.com>
2021-02-23 07:49:15 +0600

    Update 60.sql

--- sql_academy/60.sql
+++ sql_academy/60.sql
@@ -8,3 +8,15 @@ select teacher, classroom, name from schedule inner join class on class.id = sch
 select distinct h1.teacher from help h1 inner join help h2 on h1.teacher = h2.teacher where  h1.name != h2.name
 
 
+-- BETTER SOLUTION with having()
+
+with help as (
+select teacher, classroom, name from schedule inner join class on class.id = schedule.class where class.name like '11%')
+
+
+select distinct h1.teacher from help h1 inner join help h2 on h1.teacher = h2.teacher where  h1.name != h2.name
+group by h1.teacher
+having count(distinct h1.classroom) >= 1
+
+
+
