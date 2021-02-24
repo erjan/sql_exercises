369bfcbb518b9574f21a6909ce9f4b3ff2934cd9
Erjan <erjan@users.noreply.github.com>
2021-02-25 01:07:28 +0600

    Update 3-2.sql

--- интерактивный-тренажер-sql/3-2.sql
+++ интерактивный-тренажер-sql/3-2.sql
@@ -1,37 +1,51 @@
 -- В таблицу attempt включить новую попытку для студента Баранова Павла по 
 -- дисциплине «Основы баз данных». Установить текущую дату в качестве даты выполнения попытки.
-
-insert into attempt VALUES(8,1,2, NOW(), NULL);
+INSERT INTO attempt
+VALUES     (8,
+            1,
+            2,
+            Now(),
+            NULL);
 
 -- Случайным образом выбрать три вопроса (запрос) по дисциплине, тестирование по которой 
 -- собирается проходить студент, занесенный в таблицу attempt 
 -- последним, и добавить их в таблицу testing.id последней попытки получить как максимальное значение id из таблицы attempt.
-INSERT INTO testing (attempt_id, question_id)
-SELECT attempt_id, question_id
-FROM question JOIN attempt USING (subject_id)
-WHERE attempt_id = (SELECT MAX(attempt_id) FROM attempt)
-ORDER BY RAND()
-LIMIT 3;
 
-SELECT * FROM testing;
+INSERT INTO testing
+            (attempt_id,
+             question_id)
+SELECT attempt_id,
+       question_id
+FROM   question
+       JOIN attempt USING (subject_id)
+WHERE  attempt_id = (SELECT Max(attempt_id)
+                     FROM   attempt)
+ORDER  BY Rand()
+LIMIT  3;
+
+SELECT *
+FROM   testing;
 
 -- Студент прошел тестирование (то есть все его ответы занесены в таблицу testing), далее 
 -- необходимо вычислить результат(запрос) и занести его в таблицу attempt для соответствующей попытки.  Результат попытки вычислить как количество 
 -- правильных ответов, деленное на 3 (количество вопросов в каждой попытке) и умноженное на 100. Результат округлить до целого.
+UPDATE attempt
+SET    result = (SELECT Round(Sum(is_correct) / 3 * 100) AS result
+                 FROM   testing
+                        INNER JOIN answer
+                                ON testing.answer_id = answer.answer_id
+                 WHERE  attempt_id = 8)
+WHERE  attempt_id = 8;
 
-
-update attempt 
-set result = 
-(select round(sum(is_correct)/3 * 100) as result 
-from testing inner join answer on testing.answer_id = answer.answer_id where attempt_id = 8)
-where attempt_id = 8;
-
-select * from attempt;
-
+SELECT *
+FROM   attempt;
 
 -- Удалить из таблицы attempt все попытки, выполненные раньше 1 мая 2020 года. Также удалить и все соответствующие этим попыткам вопросы из таблицы testing.
+DELETE FROM attempt
+WHERE  date_attempt < '2020-05-01';
 
-delete from attempt
-where date_attempt < '2020-05-01';
-select * from attempt;
-select * from testing;
+SELECT *
+FROM   attempt;
+
+SELECT *
+FROM   testing; 
