dfb7142effffb9da8ee58ede5f7fa93d386915cb
Erjan <erjan@users.noreply.github.com>
2021-08-17 17:30:25 +0600

    Update top3_vs_bottom3.sql

--- sqplad_io/medium/top3_vs_bottom3.sql
+++ sqplad_io/medium/top3_vs_bottom3.sql
@@ -27,3 +27,31 @@ order by sales desc limit 3)
 select product_id,'bottom' as category from bottom
 union
 select product_id,'top' as category from top
+
+
+
+-- official solution
+
+WITH top AS(
+	SELECT product_id, SUM(unit_price_usd * qty)
+	FROM orders
+        WHERE order_dt >= '2021-08-01'
+        AND order_dt < '2021-09-01'
+	GROUP BY product_id
+	ORDER BY SUM(unit_price_usd * qty) DESC 
+	LIMIT 3
+), 
+bottom AS (
+	SELECT product_id, SUM(unit_price_usd * qty)
+	FROM orders
+        WHERE order_dt >= '2021-08-01'
+        AND order_dt < '2021-09-01'
+	GROUP BY product_id
+	ORDER BY SUM(unit_price_usd * qty) 
+	LIMIT 3
+)
+SELECT top.product_id, 'top' 
+FROM top
+UNION ALL
+SELECT bottom.product_id, 'bottom'
+FROM bottom;
