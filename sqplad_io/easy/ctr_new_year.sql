7677d53bbeac5c8da95bc42ee866e168017cb76c
Erjan <erjan@users.noreply.github.com>
2021-08-17 07:30:00 +0600

    Update ctr_new_year.sql

--- sqplad_io/easy/ctr_new_year.sql
+++ sqplad_io/easy/ctr_new_year.sql
@@ -2,6 +2,8 @@
 Write a query to compute the click through rate for the search results on new year's day (2021-01-01).
 Click through rate: number of searches end up with at least one click.
 Convert your result into a percentage (* 100.0).
+
+не мог понять что такое "миним. 1 клик" - это просто посчитать, я думал что там есть поиски когда был одновремено действие и клик и импресс"
 */
 
 with h1 as (
