5985e2f9d28e96b2c0e011de8e764107d0282d14
Erjan <erjan@users.noreply.github.com>
2021-08-17 07:21:55 +0600

    Create ctr_new_year.sql

---
+++ sqplad_io/easy/ctr_new_year.sql
@@ -0,0 +1,17 @@
+/*
+Write a query to compute the click through rate for the search results on new year's day (2021-01-01).
+Click through rate: number of searches end up with at least one click.
+Convert your result into a percentage (* 100.0).
+*/
+
+with h1 as (
+
+select * from search_result
+
+where extract(year from date) = 2021 and extract(day from date) = 1
+and extract(month from date) = 1)
+
+
+SELECT 
+COUNT(DISTINCT CASE WHEN action = 'click' THEN search_id ELSE NULL END) * 100.0/COUNT(DISTINCT search_id)
+from h1
