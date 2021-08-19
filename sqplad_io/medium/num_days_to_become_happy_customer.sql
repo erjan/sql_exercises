7f00f03c9fb69ae073ce25101751f4f17f4bbe2b
Erjan <erjan@users.noreply.github.com>
2021-08-19 11:22:07 +0600

    Update num_days_to_become_happy_customer.sql

--- sqplad_io/medium/num_days_to_become_happy_customer.sql
+++ sqplad_io/medium/num_days_to_become_happy_customer.sql
@@ -52,3 +52,30 @@ SELECT
 	
    WHERE tenth_rental_ts IS not NULL
 )o
+
+
+-- official solution
+
+WITH cust_rental_ts AS (
+    SELECT   
+        customer_id,
+	rental_ts,
+	ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY rental_ts) rental_idx
+    FROM rental
+)
+SELECT ROUND(AVG(delta)) AS avg_days FROM (
+    SELECT 
+        customer_id,
+	first_rental_ts,
+	tenth_rental_ts,
+	EXTRACT(DAYS FROM tenth_rental_ts - first_rental_ts) AS delta
+    FROM (
+        SELECT 
+            customer_id, 
+	    MAX(CASE WHEN rental_idx = 1 THEN rental_ts ELSE NULL END) AS first_rental_ts,
+	    MAX(CASE WHEN rental_idx = 10 THEN rental_ts ELSE NULL END) AS tenth_rental_ts
+	 FROM cust_rental_ts
+	 GROUP BY customer_id
+	) X
+    WHERE tenth_rental_ts IS NOT NULL
+)Y;
