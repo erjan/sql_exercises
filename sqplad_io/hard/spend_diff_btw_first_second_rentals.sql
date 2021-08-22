81f9a9427db303f1b61b5eae276b27b3eab4f9b6
Erjan <erjan@users.noreply.github.com>
2021-08-22 13:38:29 +0600

    Update spend_diff_btw_first_second_rentals.sql

--- sqplad_io/hard/spend_diff_btw_first_second_rentals.sql
+++ sqplad_io/hard/spend_diff_btw_first_second_rentals.sql
@@ -7,27 +7,50 @@ Skip users who only rented once.
 */
    
    
-	with help as (
 	
-select customer_id,payment_ts,amount,
-row_number()over(partition by customer_id order by payment_ts) as row_num
+	
+with help as (	
 
-from payment),
+select customer_id, payment_ts, amount,
+
+row_number() over(partition by customer_id order by payment_ts desc) as r
+
+from payment where customer_id IN (1,2,3,4,5,6,7,8,9,10) ),
+
 
 
 h2 as (
-
-select customer_id, amount, payment_ts, row_num from help
-
-where row_num <=2 and  customer_id in (1,2,3,4,5,6,7,8,9,10) ),
+  select * from help where r <=2
+  ),
 
 h3 as (
 
-select customer_id, amount - lead(amount) over(partition by customer_id order by row_num) as delta
-from h2)
+select customer_id,
 
+amount - lag(amount,1)over(partition by customer_id order by r desc)
 
-select customer_id, delta from h3 where delta is not null
+as delta
 
+from h2 )
+
+select * from h3 where delta is not null
 
 -- official solution
+
+
+
+WITH cust_spend_seq AS (
+	SELECT 
+	  customer_id,
+	  payment_ts,
+	  amount AS current_payment,
+	  LAG(amount, 1) OVER(PARTITION BY customer_id ORDER BY payment_ts) AS prev_payment,
+	  ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY payment_ts DESC) AS payment_idx
+	FROM payment P
+)
+SELECT
+    customer_id,
+    current_payment - prev_payment AS delta
+FROM cust_spend_seq
+WHERE customer_id IN(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+AND payment_idx = 1;
