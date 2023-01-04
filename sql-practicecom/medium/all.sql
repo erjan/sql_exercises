e711a507632314c8e0b315a2f76079f75be15547
Erjan <erjan@users.noreply.github.com>
2023-01-04 08:21:20 +0500

    Update all.sql

--- sql-practicecom/medium/all.sql
+++ sql-practicecom/medium/all.sql
@@ -43,8 +43,61 @@ select first_name from patients p order by len(first_name) , first_name
 
 ------------------------------------------------------------------------------------------------------------
 
+Show the total amount of male patients and the total amount of female patients in the patients table.
+Display the two results in the same row.
+
+select 
+  sum(case when gender = 'M' then 1 end) as male_count,
+  sum(case when gender = 'F' then 1 end) as female_count 
+from patients;
 
 
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------
+Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results 
+ordered ascending by allergies then by first_name then by last_name.
+
+select first_name,last_name,allergies
+ 
+ from patients where allergies in ('Penicillin' , 'Morphine')
+ 
+ order by allergies, first_name,last_name
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------
+
+
+Show patient_id, diagnosis from admissions. Find patients admitted multiple times for the same diagnosis.
+
+
+
+select 
+patient_id, diagnosis from admissions
+group by diagnosis,patient_id
+having count(*)>1
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------
+
+Show the city and the total number of patients in the city.
+Order from most to least patients and then by city name ascending.
+
+
+select 
+
+city, count(patient_id) as x from patients
+group by city
+order by x desc, city
 
 
 
@@ -54,58 +107,34 @@ select first_name from patients p order by len(first_name) , first_name
 ------------------------------------------------------------------------------------------------------------
 
 
+Show first name, last name and role of every person that is either patient or doctor.
+The roles are either "Patient" or "Doctor"
 
 
+select 
 
+ first_name,last_name,'Patient' from patients 
+ union all
+ 
+ select 
 
-
-
-------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
+ first_name,last_name,'Doctor' from doctors
 
 
 
 
 ------------------------------------------------------------------------------------------------------------
 
+Show all allergies ordered by popularity. Remove NULL values from query.
 
 
 
 
-
-
-
-
-
-------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
+select 
+allergies, count(*) as total_diagnosis
+ from patients  where allergies is not null  group by allergies order by total_diagnosis desc
+ 
+ 
 
 
 
