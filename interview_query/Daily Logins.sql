d1ae5c8b027efc75598cab3b04a03041cda668a5
Erjan <erjan@users.noreply.github.com>
2023-10-18 14:30:59 +0500

    Update Daily Logins.sql

--- interview_query/Daily Logins.sql
+++ interview_query/Daily Logins.sql
@@ -1,3 +1,13 @@
+'''
+  Let’s say we are given a table user_logins.
+
+Using this table, calculate how many users logged in an identical number of times on January 1st, 2022.
+
+For example, it may be the case that on that date three users logged in seven times, four users logged in five times, two users logged in ten times, etc.
+  '''
+
+
+
 
 with h as(
 
