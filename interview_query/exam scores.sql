8142290438c204b7ffb85726cc3bfe51df6eb103
Erjan <erjan@users.noreply.github.com>
2023-10-18 16:00:26 +0500

    Update exam scores.sql

--- interview_query/exam scores.sql
+++ interview_query/exam scores.sql
@@ -25,3 +25,13 @@ case when exam_id = 4 then score end as exam_4
 
  select student_name, max(exam_1) exam_1, max(exam_2)exam_2, 
  max(exam_3)exam_3, max(exam_4)exam_4 from h group by student_name
+
+--------------------------------
+
+SELECT student_name,
+  SUM(IF(exam_id=1, score, NULL)) AS exam_1,
+  SUM(IF(exam_id=2, score, NULL)) AS exam_2,
+  SUM(IF(exam_id=3, score, NULL)) AS exam_3,
+  SUM(IF(exam_id=4, score, NULL)) AS exam_4
+FROM exam_scores
+GROUP BY student_id;
